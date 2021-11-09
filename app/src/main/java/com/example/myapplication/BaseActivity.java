7a51d31842e7728a5d796970680a65bd65119a06
Markel <markel.garciari@elorrieta-errekamari.com>
2021-11-09 17:31:42 +0100

    Change Password functionality

--- app/src/main/java/com/example/myapplication/BaseActivity.java
+++ app/src/main/java/com/example/myapplication/BaseActivity.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -33,6 +39,44 @@ public class BaseActivity extends AppCompatActivity {
             finish();
         } );
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.changePassword) {
+
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            EditText newContra = new EditText(this);
+            alert.setMessage("Introduzca la nueva contraseña");
+            alert.setTitle("Cambiar contraseña");
+
+            alert.setView(newContra);
+
+            alert.setPositiveButton("Cambiar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    //What ever you want to do with the value
+                    String password = newContra.getText().toString();
+
+                    Context contexto = getApplicationContext();
+                    DBManager db = new DBManager(contexto);
+                    SharedPreferences sharedPref = contexto.getSharedPreferences( "temp", Context.MODE_PRIVATE );
+
+                    db.updatePassword(new Usuario(sharedPref.getString("session" , null), password));
+                }
+            });
+
+            alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                }
+            });
+
+            alert.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
